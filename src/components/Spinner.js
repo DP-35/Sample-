0b6bd187eda0c44bf20ba09e706348a803ee70f4
DP-35 <66679701+DP-35@users.noreply.github.com>
2022-10-31 19:31:27 +0530

    Create Spinner.js

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import loading from './loading.gif'
+
+const Spinner=(props) => {  // export default class Spinner extends Component {
+  // render() {
+    return (
+      <div className='text-center'>
+        <img src={loading} alt="loading"/>
+      </div>
+    )
+  // }
+}
+
+export default Spinner
